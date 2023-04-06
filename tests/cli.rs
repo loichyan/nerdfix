2eb1a6e564d2ebc06eca3b6d83cf70db4f0cb92e
loichyan <loichyan@foxmail.com>
2023-04-06 16:26:58 +0800

    test(cli): add some cli tests

---
+++ tests/cli.rs
@@ -0,0 +1,55 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::{path::Path, process::Command};
+
+fn cmp_or_override(name: &str) -> impl '_ + Predicate<[u8]> {
+    predicate::function(move |content| {
+        let path = format!("tests/cli/{name}.stderr");
+        let path = Path::new(&path);
+        let content = strip_ansi_escapes::strip(content).unwrap();
+        if path.exists() {
+            let expected = std::fs::read(path).unwrap();
+            content == expected
+        } else {
+            std::fs::write(path, content).unwrap();
+            true
+        }
+    })
+}
+
+#[test]
+fn check() {
+    Command::cargo_bin("nerdfix")
+        .unwrap()
+        .arg("check")
+        .arg("tests/test-data.txt")
+        .assert()
+        .success()
+        .stderr(cmp_or_override("check"));
+}
+
+#[test]
+fn check_with_input() {
+    Command::cargo_bin("nerdfix")
+        .unwrap()
+        .arg("-i")
+        .arg("tests/test-input.txt")
+        .arg("check")
+        .arg("tests/test-data.txt")
+        .assert()
+        .success()
+        .stderr(cmp_or_override("check_with_input"));
+}
+
+#[test]
+fn check_json() {
+    Command::cargo_bin("nerdfix")
+        .unwrap()
+        .arg("check")
+        .arg("--format")
+        .arg("json")
+        .arg("tests/test-data.txt")
+        .assert()
+        .success()
+        .stderr(cmp_or_override("check_json"));
+}
