a9a3630c6eafe6558fcca49aa964243d0f5b688f
loichyan <loichyan@foxmail.com>
2023-05-04 14:48:34 +0800

    build(nix): simplify nix derivation

--- flake.nix
+++ flake.nix
@@ -6,14 +6,9 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
-  outputs = { nixpkgs, flake-utils, fenix, crane, ... }:
+  outputs = { nixpkgs, flake-utils, fenix, ... }:
     let
       mkPkgs = pkgs:
         rec {
@@ -21,19 +16,16 @@
             stable.defaultToolchain
             stable.rust-src
           ]);
-          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          rustPlatform = (pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          });
           nerdfix =
-            let
-              craneFilter = path: type:
-                path != "cache-version"
-                || (craneLib.filterCargoSources path type);
-              inherit (pkgs.lib) cleanSourceWith;
-            in
-            craneLib.buildPackage {
-              src = cleanSourceWith {
-                src = (craneLib.path ./.);
-                filter = craneFilter;
-              };
+            rustPlatform.buildRustPackage {
+              pname = "nerdfix";
+              version = "0.2.3";
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
             };
         }
       ;
